ea7443b45a8e61a24293522977d3375ad1cec134
Exonize <5884535@rts-365.dk>
2019-05-28 13:26:48 +0200

    --

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,4 +4,16 @@ module.exports = (app) => {
       res.render('home');
    });
 
+   app.get('/', (req, res, next) => {
+      res.render('singlepost');
+   });
+
+   app.get('/', (req, res, next) => {
+      res.render('scripts');
+   });
+
+   app.get('/', (req, res, next) => {
+      res.render('home');
+   });
+
 };
