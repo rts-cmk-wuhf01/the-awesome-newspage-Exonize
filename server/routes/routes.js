00dbb3f7a94611d09ccf82faf69fee8a46f93123
Exonize <5884535@rts-365.dk>
2019-06-19 13:07:31 +0200

    Comments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,19 @@
-const mysql = require('../config/mysql.js');
+  // Connect to Database
+  const mysql = require('../config/mysql.js');
 
-async function getCategories() {
-   let db = await mysql.connect();
-   let [categories] = await db.execute(`
-      SELECT category_id, category_title 
-      FROM categories
-      ORDER BY category_title ASC`);
-   db.end();
-   return categories;
-}
-
-module.exports = (app) => {
+  // Get Categories
+   async function getCategories() {
+      let db = await mysql.connect();
+      let [categories] = await db.execute(`
+         SELECT category_id, category_title 
+         FROM categories
+         ORDER BY category_title ASC`);
+      db.end();
+      return categories;
+   }
+   //=========================================================================
+   // Module Exports
+   module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
       res.render('home');
@@ -24,24 +27,22 @@ module.exports = (app) => {
       res.render('about');
    });
 
-
-
-
-
+   //=========================================================================
+   // Contact Form
    app.get('/contact', (req, res, next) => {
       res.render('contact');
    });   
 
    app.post('/contact', async (req, res, next) => {
 
-      // indsamling af værdierne og oprettelse af de nødvendige variabler.
+      // Collection of values and necessary variables
       let name = req.body.name;
       let email = req.body.email;
       let subject = req.body.subject;
       let message = req.body.message;
       let contactDate = new Date();
 
-      // håndter valideringen, alle fejl pushes til et array så de er samlet ET sted
+      // Validate form - Pushes all errors to one array
       let return_message = [];
       if (name == undefined || name == '') {
          return_message.push('Navn mangler');
@@ -57,12 +58,11 @@ module.exports = (app) => {
       }
 
       if (return_message.length > 0) {
-         // der er mindst 1 information der mangler, returner beskeden som en string.
-         let categories = await getCategories(); // denne forklares lige om lidt!
+         let categories = await getCategories(); 
          res.render('contact', {
             'categories': categories,
             'return_message': return_message.join(', '),
-            'values': req.body // læg mærke til vi "bare" sender req.body tilbage
+            'values': req.body // Returning Req.Body
          });
          
       } else {
@@ -79,14 +79,14 @@ module.exports = (app) => {
 
          console.log(result[0])
 
-         // affected rows er større end nul, hvis en (eller flere) række(r) blev indsat
+         // Affected Rows is bigger than 0 - If 1 or more rows got inserted
          if (result[0].affectedRows > 0) {
             return_message.push('Tak for din besked, vi vender tilbage hurtigst muligt');
          } else {
             return_message.push('Din besked blev ikke modtaget.... ');
          }
 
-         let categories = await getCategories(); // denne har jeg ikke forklaret endnu! 
+         let categories = await getCategories();
          res.render('contact', {
             'categories': categories,
             'return_message': return_message.join(', '),
@@ -94,26 +94,12 @@ module.exports = (app) => {
          });
        } catch (error) {
           console.log(error.message)
-       } db.end();
-      }
+      } db.end();
+   }
    });
-
+   //=========================================================================
    
-
-
-
-
-
-
-
-
-
-
-
-
-
    app.get('/category/:category_id', async (req, res, next) => {
-
 
    let db = await mysql.connect();
    let [categories] = await db.execute('SELECT * FROM categories WHERE category_id = ?', [1]);
@@ -137,6 +123,8 @@ module.exports = (app) => {
       INNER JOIN authors ON author_id = fk_author_id
       WHERE fk_category_id = ?`, [req.params.category_id]);
 
+   //=========================================================================
+
    let [latestPosts] = await db.execute(`
    SELECT 
          category_id
@@ -155,12 +143,16 @@ module.exports = (app) => {
       LIMIT 1)
       ORDER BY article_postdate DESC`);
 
+   //=========================================================================
+
    let [comments] = await db.execute(`
       SELECT article_title, 
       comment_postdate, 
       comment_text, 
       FROM comments 
       WHERE fk_article_id = ?`, [req.params.article_id]);
+
+   //=========================================================================
 
       console.log(categories)
       res.render('categories-post' , {
@@ -173,6 +165,8 @@ module.exports = (app) => {
       db.end();
    });
 
+   //=========================================================================
+
    app.get('/categories-post', (req, res, next) => {
       res.render('categories-post');
    });
@@ -180,5 +174,7 @@ module.exports = (app) => {
    app.get('/categories-post/:category_id', async (req, res, next) => {
       res.send(req.params.category_id);
    });
+
+   //=========================================================================
    
-};
+}; // Module Exports End
